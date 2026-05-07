97cebc9d2ae5db6e000b3eed188857d65c600a69
Artem Suprun <efogdev@gmail.com>
2026-05-08 02:59:22 +0300

    feat: move feedback to a separate module

--- src/behaviors/behavior_switch_keymap.c
+++ src/behaviors/behavior_switch_keymap.c
@@ -4,11 +4,13 @@
 #include <zephyr/device.h>
 #include <zephyr/logging/log.h>
 #include "drivers/behavior.h"
-#include "zephyr/drivers/gpio.h"
 #include "zephyr/logging/log.h"
 #include "zephyr/settings/settings.h"
 #include "zmk/behavior.h"
 #include "drivers/keymap_shell.h"
+#if IS_ENABLED(CONFIG_ZMK_FEEDBACK_COMMON)
+#include <zmk/feedback_common/feedback_gpio.h>
+#endif
 
 #define DT_DRV_COMPAT zmk_behavior_switch_keymap
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
@@ -18,15 +20,7 @@ static uint8_t g_dev_num = 0;
 static const char* g_devices[CONFIG_ZMK_KEYMAP_SHELL_SLOTS] = { NULL };
 
 struct behavior_switch_keymap_config {
-    const struct gpio_dt_spec feedback_gpios;
-    const struct gpio_dt_spec feedback_extra_gpios;
     const uint32_t feedback_duration;
-};
-
-struct behavior_switch_keymap_data {
-    const struct device *dev;
-    struct k_work_delayable feedback_off_work;
-    int previous_feedback_extra_state;
 };
 
 #if IS_ENABLED(CONFIG_ZMK_BEHAVIOR_METADATA)
@@ -55,7 +49,6 @@ static const struct behavior_parameter_metadata metadata = { .sets_len = ARRAY_S
 static int on_skmp_binding_pressed(struct zmk_behavior_binding *binding, struct zmk_behavior_binding_event event) {
     const struct device* dev = zmk_behavior_get_binding(binding->behavior_dev);
     const struct behavior_switch_keymap_config *cfg = dev->config;
-    struct behavior_switch_keymap_data *data = dev->data;
     const uint8_t slot_idx = binding->param1 - 1;
     int err = 0;
 
@@ -65,68 +58,19 @@ static int on_skmp_binding_pressed(struct zmk_behavior_binding *binding, struct 
         err = keymap_shell_activate_slot(slot_idx);
     }
 
-    if (err == 0) {
-        if (cfg->feedback_gpios.port != NULL) {
-            if (cfg->feedback_extra_gpios.port != NULL) {
-                data->previous_feedback_extra_state = gpio_pin_get_dt(&cfg->feedback_extra_gpios);
-                gpio_pin_set_dt(&cfg->feedback_extra_gpios, 1);
-            }
-            gpio_pin_set_dt(&cfg->feedback_gpios, 1);
-            
-            if (cfg->feedback_duration > 0) {
-                k_work_reschedule(&data->feedback_off_work, K_MSEC(cfg->feedback_duration));
-            }
-        }
+#if IS_ENABLED(CONFIG_ZMK_FEEDBACK_COMMON)
+    if (err == 0 && cfg->feedback_duration > 0) {
+        fbc_trigger(cfg->feedback_duration);
     }
+#else
+    ARG_UNUSED(err);
+    ARG_UNUSED(cfg);
+#endif
 
     return ZMK_BEHAVIOR_OPAQUE;
 }
 
-static void feedback_off_work_cb(struct k_work *work) {
-    struct k_work_delayable *dwork = k_work_delayable_from_work(work);
-    const struct behavior_switch_keymap_data *data = CONTAINER_OF(dwork, struct behavior_switch_keymap_data, feedback_off_work);
-    const struct device *dev = data->dev;
-    const struct behavior_switch_keymap_config *config = dev->config;
-
-    if (config->feedback_extra_gpios.port != NULL) {
-        gpio_pin_set_dt(&config->feedback_extra_gpios, data->previous_feedback_extra_state);
-    }
-
-    if (config->feedback_gpios.port != NULL) {
-        gpio_pin_set_dt(&config->feedback_gpios, 0);
-    }
-
-    LOG_DBG("Feedback turned off, extra GPIOs restored to previous state");
-}
-
 static int behavior_switch_keymap_init(const struct device *dev) {
-    const struct behavior_switch_keymap_config *cfg = dev->config;
-    struct behavior_switch_keymap_data *data = dev->data;
-    data->previous_feedback_extra_state = 0;
-
-    if (cfg->feedback_gpios.port != NULL) {
-        if (gpio_pin_configure_dt(&cfg->feedback_gpios, GPIO_OUTPUT) != 0) {
-            LOG_WRN("Failed to configure rate limit feedback GPIO");
-        } else {
-            LOG_DBG("Rate limit feedback GPIO configured");
-        }
-
-        k_work_init_delayable(&data->feedback_off_work, feedback_off_work_cb);
-    } else {
-        LOG_DBG("No feedback set up for rate limit cycling");
-    }
-
-    if (cfg->feedback_extra_gpios.port != NULL) {
-        if (gpio_pin_configure_dt(&cfg->feedback_extra_gpios, GPIO_OUTPUT) != 0) {
-            LOG_WRN("Failed to configure rate limit extra feedback GPIO");
-        } else {
-            LOG_DBG("Rate limit extra feedback GPIO configured");
-        }
-    } else {
-        LOG_DBG("No extra feedback set up for rate limit cycling");
-    }
-
-    data->dev = dev;
     g_devices[g_dev_num++] = dev->name;
     return 0;
 }
@@ -139,13 +83,10 @@ static const struct behavior_driver_api behavior_switch_keymap_driver_api = {
 };
 
 #define SKMP_INST(n)                                                                                  \
-    static struct behavior_switch_keymap_data behavior_switch_keymap_data_##n = {};                            \
     static const struct behavior_switch_keymap_config behavior_switch_keymap_config_##n = {                    \
-        .feedback_gpios = GPIO_DT_SPEC_INST_GET_OR(n, feedback_gpios, { .port = NULL }),                 \
-        .feedback_extra_gpios = GPIO_DT_SPEC_INST_GET_OR(n, feedback_extra_gpios, { .port = NULL }),     \
         .feedback_duration = DT_INST_PROP_OR(n, feedback_duration, 0),                                   \
     };                                                                                                   \
-    BEHAVIOR_DT_INST_DEFINE(n, behavior_switch_keymap_init, NULL, &behavior_switch_keymap_data_##n,            \
+    BEHAVIOR_DT_INST_DEFINE(n, behavior_switch_keymap_init, NULL, NULL,                                  \
         &behavior_switch_keymap_config_##n, POST_KERNEL, CONFIG_KERNEL_INIT_PRIORITY_DEFAULT, &behavior_switch_keymap_driver_api);
 
 DT_INST_FOREACH_STATUS_OKAY(SKMP_INST)
